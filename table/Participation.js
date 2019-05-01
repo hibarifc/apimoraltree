0cd983c3f733e26efa21b8a8d6a2bb5de0293991
hibarifc <suriya181@hotmail.com>
2019-05-01 10:39:51 +0700

    Signed-off-by: hibarifc <suriya181@hotmail.com>

--- table/Participation.js
+++ table/Participation.js
@@ -78,19 +78,46 @@ exports.deleteParticipationById = function (req, res) {
 }
 
 exports.getParticipationById = function (req, res) {
-    var sqlRequest = "SELECT SUM(Participation_Activity.score)as score FROM Participation_Activity LEFT JOIN Activity ON Activity.Act_ID = Participation_Activity.Act_ID  WHERE Member_ID = $Member_ID";
+    var sqlRequest = "SELECT SUM(Participation_Activity.score)as score,Participation_Activity.Tree_ID  FROM Participation_Activity LEFT JOIN Activity ON Activity.Act_ID = Participation_Activity.Act_ID  WHERE Member_ID = $Member_ID";
     let sqlParams = {
         $Member_ID:req.body.Member_ID,
     };
     let stmt = database.db.prepare(sqlRequest);
     stmt.all(sqlParams, function (err,rows) {
-    
+        
         if (err) {
             res.json({ status: false, data: "Internal server error"});
         } else if (rows == null || rows.length == 0) {
             res.json({ status: false, data: "Entity not found" });
         } else {
-            res.json({ status: true, data: rows });
+            var level;
+            var score = rows[0]['score'];
+            var Tree_ID = rows[0]['Tree_ID'];
+            if(score<25){
+                level = 1
+            }else if(score>=25&&score<50){
+                level = 2
+            }else if(score>=50&&score<75){
+                level = 3
+            }else{
+                level = 4
+            }
+            var sqlRequest = "SELECT * FROM Tree_Growth LEFT JOIN Moral_Tree on Moral_Tree.Tree_ID = Tree_Growth.Tree_ID WHERE Tree_Growth.Level_ID = $level AND Tree_Growth.Tree_ID = $Tree_ID ";
+            let sqlParams = {
+                $level:level,
+                $Tree_ID:Tree_ID
+            };
+            let stmt = database.db.prepare(sqlRequest);
+            stmt.all(sqlParams, function (err,rows) {
+            
+                if (err) {
+                    res.json({ status: false, data: "Internal server error"});
+                } else if (rows == null || rows.length == 0) {
+                    res.json({ status: false, data: "Entity not found" });
+                } else {
+                    res.json({ status: true, score: score,treepath:rows[0]['Tree_Photo'],treename:rows[0]['Tree_Name'] });
+                }
+            });
         }
     });
 }
@@ -131,7 +158,22 @@ exports.getRanking = function (req, res) {
         } else if (rows === null || rows.length === 0) {
             res.json({ status: false, data: "Entity not found" });
         } else {
-            res.json({ status: true, data: rows });
+            var tableone =rows;
+            var sqlRequest = `SELECT Moral.Moral_Name,Activity.Act_Name,SUM(Participation_Activity.score) as score FROM Participation_Activity
+            LEFT JOIN Moral_Activity on Moral_Activity.Act_ID = Participation_Activity.Act_ID
+            LEFT JOIN Activity on Activity.Act_ID = Moral_Activity.Act_ID
+            LEFT JOIN Moral on Moral.Moral_ID = Moral_Activity.Moral_ID
+            GROUP BY Moral_Activity.Act_ID `;
+            database.db.all(sqlRequest, function (err, rows) {
+                if (err) {
+                    res.json({ status: false, data: "Internal server error"});
+                } else if (rows === null || rows.length === 0) {
+                    res.json({ status: false, data: "Entity not found" });
+                } else {
+                    res.json({ status: true, tableone: tableone,tabletwo:rows });
+                }
+            })
+            
         }
     })
     
